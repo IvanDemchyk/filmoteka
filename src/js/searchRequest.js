5e3b6fa161400daf29e14c4a3c0a8da70c678b90
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-01-27 03:43:34 +0200

    Update searchRequest.js

--- src/js/searchRequest.js
+++ src/js/searchRequest.js
@@ -5,17 +5,18 @@ import { CURRENT_MOVIES, WATCHE, QUEUE, watche, queue } from './local';
 const form = document.querySelector('.form-js');
 const inputEl = document.querySelector('.form-input');
 const notif = document.querySelector('.form__notification');
-
+let page = 1;
 async function inputRequest(e) {
   e.preventDefault();
+  console.log('hello');
   const request = await inputEl.value.trim();
   if (!request) {
     console.log('Error1');
     return;
   }
   try {
-    page = 1;
     const data = await fetchMovies(page, request);
+    console.log('hello try');
     if (data.results.length === 0) {
       console.dir(notif.style.visibility);
       notif.style.visibility = 'visible';
