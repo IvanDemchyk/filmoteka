5d1fd714edfccb636a6a523c79c09e42882d0898
VVladyslav <VVVasylyshyn@gmail.com>
2023-01-28 21:54:42 +0200

    Genres to local storage
    fetches movie genres list as array of objects from resors and save  in LS as an object with properties

--- src/js/get-genres.js
+++ src/js/get-genres.js
@@ -1,4 +1,4 @@
-console.log('genres');const GENRES_MOVIES = 'genresList';
+const GENRES_MOVIES = 'genresList';
 const fetchedGenres = () => {
   return fetch(
     `https://api.themoviedb.org/3/genre/movie/list?api_key=e8369cced440ebe3b010b622661df24b&language=en-US`
@@ -8,7 +8,6 @@ const fetchedGenres = () => {
 fetchedGenres().then(onFetchSuccess);
 
 function onFetchSuccess(respond) {
-  console.log(respond.genres);
   const genresList = respond.genres.reduce((acc, { id, name }) => {
     acc[id] = name;
     return acc;
