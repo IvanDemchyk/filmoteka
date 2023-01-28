e13c009c02aea7f92bfdbf0466ff69f2d1ec8ac8
marinamelnik1105 <masha.nasha119@gmail.com>
2023-01-28 10:34:17 +0200

    add switching active button on click

--- src/js/queue.js
+++ src/js/queue.js
@@ -2,6 +2,9 @@ const watchedBtn = document.querySelector('.watched-btn-js');
 const queueBtn = document.querySelector('.queue-btn-js');
 queueBtn.addEventListener('click', onClickQueueBtn);
 function onClickQueueBtn() {
+  onActiveQueueBtn();
+}
+function onActiveQueueBtn() {
   queueBtn.classList.add('active');
   watchedBtn.classList.remove('active');
 }
--- src/js/watched.js
+++ src/js/watched.js
@@ -2,6 +2,9 @@ const watchedBtn = document.querySelector('.watched-btn-js');
 const queueBtn = document.querySelector('.queue-btn-js');
 watchedBtn.addEventListener('click', onClickWatchedBtn);
 function onClickWatchedBtn() {
+  onActiveWatchedBtn();
+}
+function onActiveWatchedBtn() {
   watchedBtn.classList.add('active');
   queueBtn.classList.remove('active');
 }
