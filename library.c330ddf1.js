66ff58eb3148855396c4c0d1a5c861e41371073f
IvanDemchyk <110597699+IvanDemchyk@users.noreply.github.com>
2023-01-28 16:07:29 +0000

    Deploying to gh-pages from @ IvanDemchyk/filmoteka@cacedd717ca5d9d459845594c72de2cd661666cb 🚀

---
+++ library.c330ddf1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".watched-btn-js"),t=document.querySelector(".queue-btn-js");e.addEventListener("click",(function(){e.classList.add("active"),t.classList.remove("active")}));const c=document.querySelector(".watched-btn-js"),s=document.querySelector(".queue-btn-js");s.addEventListener("click",(function(){s.classList.add("active"),c.classList.remove("active")}));
+//# sourceMappingURL=library.c330ddf1.js.map
