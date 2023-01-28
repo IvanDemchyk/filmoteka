991096b023011318b7448e97f62869c8644381f3
Artem Riabchych <artemskyart@gmail.com>
2023-01-28 21:23:31 +0200

    Fix-1

--- src/js/fetchTrends.js
+++ src/js/fetchTrends.js
@@ -1,15 +1,9 @@
-// console.log("api-films")
-// export default function getFetchMovies(p) {
-//   return fetch(
-//     `https://api.themoviedb.org/3/trending/movie/week?api_key=0b11624b950ea9c4284f61844023b09c&page=${p}`
-//   ).then(data => data.json());
-// }
 
 import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const KEY_API = '0b11624b950ea9c4284f61844023b09c';
 // let page = 1;
-export async function getfetchTrends(page, request) {
+export async function fetchTrends(page, request) {
   try {
     const response = await axios.get(
       `${BASE_URL}trending/movie/week?api_key=${KEY_API}&page=${page}`
--- src/js/get-films.js
+++ src/js/get-films.js
@@ -1,20 +1,20 @@
 // console.log('get-films');
 import { createMovieCard } from './createMovieCard.js';
-import { getfetchTrends } from './api-films.js';
+import { getfetchTrends } from './fetchTrends.js';
 import { CURRENT_MOVIES, WATCHE, QUEUE, watche, queue } from './local.js';
 import { card } from './local';
 import { globalRequest } from './searchRequest';
-console.log(globalRequest);
-let page = 1;
+// console.log(globalRequest);
+// let page = 1;
 let btnActionWatch = 'add to watched';
-async function render(page) {
-  const data = await getfetchTrends(page);
-  localStorage.setItem(CURRENT_MOVIES, JSON.stringify(data));
-  card.innerHTML = createMovieCard(
-    JSON.parse(localStorage.getItem(CURRENT_MOVIES)).results
-  );
-}
-render(page);
+// async function render(page) {
+//   const data = await getfetchTrends(page);
+//   localStorage.setItem(CURRENT_MOVIES, JSON.stringify(data));
+//   card.innerHTML = createMovieCard(
+//     JSON.parse(localStorage.getItem(CURRENT_MOVIES)).results
+//   );
+// }
+// render(page);
 
 //Додаю сюди для перевірки функціоналу, поки без підключення
 //Модальне вікно
