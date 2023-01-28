0a0729b4de44bcf9508af3c7ab0ea9b44d40b3b4
IvanDemchyk <ivandem4ik@gmail.com>
2023-01-28 22:35:02 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './js/get-films.js';
+// import './js/get-films.js';
 import './js/searchRequest';
 import './js/get-genres';
 import './js/pagination';
+import './js/modal-window';
--- src/js/get-films.js
+++ src/js/get-films.js
@@ -4,8 +4,7 @@ import { getfetchTrends } from './fetchTrends.js';
 import { CURRENT_MOVIES, WATCHE, QUEUE, watche, queue } from './local.js';
 import { card } from './local';
 import { globalRequest } from './searchRequest';
-
-
+import { showMovie } from './modal-window.js';
 //Додаю сюди для перевірки функціоналу, поки без підключення
 //Модальне вікно
 card.addEventListener('click', showMovie);
