59ce6dcde68d26c411f8e0313fb0067df88e9f83
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-01-29 21:09:55 +0200

    update styles

--- src/js/libaryLocal.js
+++ src/js/libaryLocal.js
@@ -3,14 +3,17 @@ import { createMovieCard } from './createMovieCard';
 import { card } from './local';
 
 const divContainer = document.querySelector('#main');
+const header = document.querySelector('.library-header');
 
 export function checkDataRenderPage(data) {
   if (!data || !data.length) {
     divContainer.classList.add('notification-bcg');
+    header.classList.add('library-header-notification');
     return `<p class="notification-desc">
             Nothing here yet, go back and select a movie.
             </p>`;
   }
+  header.classList.remove('library-header-notification');
   divContainer.classList.remove('notification-bcg');
   return createMovieCard(data);
 }
